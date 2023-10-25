19de3e6e6409d17fc4afde975cc58e9530ec659c
QJia <qianjia@huansi.net>
2023-10-25 14:28:34 +0800

    chore: 代理配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 /// <reference types="vitest" />
 
 import path from 'node:path'
-import { defineConfig } from 'vite'
+import process from 'node:process'
+import { defineConfig, loadEnv } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
 import Components from 'unplugin-vue-components/vite'
@@ -9,55 +10,73 @@ import AutoImport from 'unplugin-auto-import/vite'
 import UnoCSS from 'unocss/vite'
 import VueMacros from 'unplugin-vue-macros/vite'
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@/': `${path.resolve(__dirname, 'src')}/`,
-    },
-  },
-  plugins: [
-    VueMacros({
-      defineOptions: false,
-      defineModels: false,
-      plugins: {
-        vue: Vue({
-          script: {
-            propsDestructure: true,
-            defineModel: true,
-          },
-        }),
+export default defineConfig(({ command, mode }) => {
+  // 根据当前工作目录中的 `mode` 加载 .env 文件
+  // 设置第三个参数为 '' 来加载所有环境变量，而不管是否有 `VITE_` 前缀。
+  const { NODE_ENV, VITE_APP_SERVER_URL, VITE_BASE_URL } = loadEnv(mode, process.cwd(), '')
+
+  console.log(`vite.config.ts: ${command} ${mode}`)
+  console.log(NODE_ENV, VITE_APP_SERVER_URL, 'env')
+
+  return {
+    resolve: {
+      alias: {
+        '@/': `${path.resolve(__dirname, 'src')}/`,
       },
-    }),
+    },
+    plugins: [
+      VueMacros({
+        defineOptions: false,
+        defineModels: false,
+        plugins: {
+          vue: Vue({
+            script: {
+              propsDestructure: true,
+              defineModel: true,
+            },
+          }),
+        },
+      }),
 
-    // https://github.com/hannoeru/vite-plugin-pages
-    Pages(),
+      // https://github.com/hannoeru/vite-plugin-pages
+      Pages(),
 
-    // https://github.com/antfu/unplugin-auto-import
-    AutoImport({
-      imports: [
-        'vue',
-        'vue-router',
-        '@vueuse/core',
-      ],
-      dts: true,
-      dirs: [
-        './src/composables',
-      ],
-      vueTemplate: true,
-    }),
+      // https://github.com/antfu/unplugin-auto-import
+      AutoImport({
+        imports: [
+          'vue',
+          'vue-router',
+          '@vueuse/core',
+        ],
+        dts: true,
+        dirs: [
+          './src/composables',
+        ],
+        vueTemplate: true,
+      }),
 
-    // https://github.com/antfu/vite-plugin-components
-    Components({
-      dts: true,
-    }),
+      // https://github.com/antfu/vite-plugin-components
+      Components({
+        dts: true,
+      }),
 
-    // https://github.com/antfu/unocss
-    // see uno.config.ts for config
-    UnoCSS(),
-  ],
+      // https://github.com/antfu/unocss
+      // see uno.config.ts for config
+      UnoCSS(),
+    ],
 
-  // https://github.com/vitest-dev/vitest
-  test: {
-    environment: 'jsdom',
-  },
+    server: {
+      proxy: {
+        [VITE_BASE_URL]: {
+          target: VITE_APP_SERVER_URL,
+          changeOrigin: true,
+          rewrite: path => path.replace(new RegExp(`^${VITE_BASE_URL}`), ''),
+        },
+      },
+    },
+    // https://github.com/vitest-dev/vitest
+    test: {
+      environment: 'jsdom',
+    },
+  }
 })
